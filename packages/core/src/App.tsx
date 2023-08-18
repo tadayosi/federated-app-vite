48a3f40aa790761770d1cedc59447970288e4c7c
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-08-18 21:51:05 +0900

    transform to monorepo

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -6,5 +6,3 @@ export const App = () => {
     </div>
   )
 }
-
-export default App
---
+++ packages/plugin1/src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>Plugin 1</h1>
+      <p>Hello!</p>
+    </div>
+  )
+}
